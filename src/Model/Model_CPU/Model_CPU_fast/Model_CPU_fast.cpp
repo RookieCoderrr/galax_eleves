d347ac3a334772b2328afa9de744e2acd3781145
Ni Qiang <ni.qiang@imt-atlantique.net>
2023-03-10 14:31:22 +0100

    add xsimd for velocities and positions

--- src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
+++ src/Model/Model_CPU/Model_CPU_fast/Model_CPU_fast.cpp
@@ -143,14 +143,32 @@ void Model_CPU_fast
 	auto& particlesx = particles.x;
     auto& particlesy = particles.y;
     auto& particlesz = particles.z;
+	#pragma omp parallel for
 	for (int i = 0; i < n_particles; i++)
 	{
-		velocitiesx[i] += accelerationsx[i] * 2.0f;
-		velocitiesy[i] += accelerationsy[i] * 2.0f;
-		velocitiesz[i] += accelerationsz[i] * 2.0f;
-		particlesx[i] += velocitiesx   [i] * 0.1f;
-		particlesy[i] += velocitiesy   [i] * 0.1f;
-		particlesz[i] += velocitiesz   [i] * 0.1f;
+		b_type rposx_i = b_type::load_unaligned(&particlesx[i]);
+        b_type rposy_i = b_type::load_unaligned(&particlesy[i]);
+        b_type rposz_i = b_type::load_unaligned(&particlesz[i]);
+		b_type rvelx_i = b_type::load_unaligned(&velocitiesx[i]);
+		b_type rvely_i = b_type::load_unaligned(&velocitiesy[i]);
+		b_type rvelz_i = b_type::load_unaligned(&velocitiesz[i]);
+		const b_type raccx_i = b_type::load_unaligned(&accelerationsx[i]);
+        const b_type raccy_i = b_type::load_unaligned(&accelerationsy[i]);
+        const b_type raccz_i = b_type::load_unaligned(&accelerationsz[i]);
+
+		rvelx_i += raccx_i * 2.0f;
+		rvely_i += raccy_i * 2.0f;
+		rvelz_i += raccz_i * 2.0f;
+		rposx_i += rvelx_i * 0.1f;
+		rposy_i += rvely_i * 0.1f;
+		rposz_i += rvelz_i * 0.1f;
+
+		rvelx_i.store_unaligned(&velocitiesx[i]);
+		rvely_i.store_unaligned(&velocitiesy[i]);
+		rvelz_i.store_unaligned(&velocitiesz[i]);
+		rposx_i.store_unaligned(&particlesx[i]);
+		rposy_i.store_unaligned(&particlesy[i]);
+		rposz_i.store_unaligned(&particlesz[i]);
 	}
 #endif
 
